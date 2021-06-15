b00eea2100984379c226276839d1ef8ba2b3df2f
pdcook <qphysicscook@gmail.com>
2021-06-15 17:15:23 -0400

    initial commit

---
+++ CrosshairForAll/CrosshairForAll.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using BepInEx;
+using UnboundLib;
+using HarmonyLib;
+using UnityEngine;
+
+namespace CrosshairForAll 
+{
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInPlugin("pykess-and-ascyst.rounds.plugins.crosshairforall", "Crosshair For All", "0.0.0.0")]
+    [BepInProcess("Rounds.exe")]
+    public class CrosshairForAll : BaseUnityPlugin
+    {
+    }
+}
