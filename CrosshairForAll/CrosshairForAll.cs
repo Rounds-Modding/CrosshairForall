f2a60687bb2ceca96d1b09c2726eb005b09e6051
pdcook <qphysicscook@gmail.com>
2021-06-15 23:30:08 -0400

    added assetbundles

--- CrosshairForAll/CrosshairForAll.cs
+++ CrosshairForAll/CrosshairForAll.cs
@@ -3,6 +3,7 @@ using BepInEx;
 using UnboundLib;
 using HarmonyLib;
 using UnityEngine;
+using Jotunn.Utils;
 
 namespace CrosshairForAll 
 {
@@ -11,5 +12,72 @@ namespace CrosshairForAll
     [BepInProcess("Rounds.exe")]
     public class CrosshairForAll : BaseUnityPlugin
     {
+        private void Awake()
+        {
+            new Harmony("pykess-and-ascyst.rounds.plugins.crosshairforall").PatchAll();
+        }
+        private void Start()
+        {
+            // load asset
+            //CrosshairForAll.Assets = AssetUtils.LoadAssetBundleFromResources("crosshairwhite", typeof(CrosshairForAll).Assembly);
+            //CrosshairForAll.Assets = AssetUtils.LoadAssetBundleFromResources("pceassetbundle", typeof(CrosshairForAll).Assembly); // test asset bundle while waiting for Ascyst
+
+        }
+
+        private const string ModId = "pykess-and-ascyst.rounds.plugins.crosshairforall";
+        private const string ModName = "Crosshair For All";
+        internal static AssetBundle Assets;
     }
+
+    [Serializable]
+    [HarmonyPatch(typeof(Gun), "Start")]
+    class GunPatchStart
+    {
+        private static void Postfix(Gun __instance)
+        {
+            // all of these give null reference exceptions
+            //Player player = __instance.holdable.holder.player;
+            //Player player = __instance.GetComponentInParent<Player>();
+            //Player player = __instance.player;
+            //GeneralInput input = player.GetComponent<GeneralInput>();
+
+            // don't modify keyboard players
+            //if (input.inputType == GeneralInput.InputType.Keyboard) { return; }
+
+            // crosshair doesn't move with the player, the gun, or really at all
+            GameObject crosshair = GameObject.Instantiate(CrosshairForAll.Assets.LoadAsset<GameObject>("crosshair"), __instance.transform.position, __instance.transform.rotation, __instance.transform);
+            ControllerCrosshair controller = crosshair.gameObject.GetOrAddComponent<ControllerCrosshair>();
+            crosshair.transform.localPosition = Vector3.zero;
+            crosshair.transform.SetAsFirstSibling();
+            crosshair.transform.localScale = Vector3.one;
+            //crosshair.transform.SetParent(__instance.transform);
+
+        }
+    }
+
+    public class ControllerCrosshair : MonoBehaviour
+    {
+        void Awake()
+        {
+
+        }
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+        }
+        public void OnDestroy()
+        {
+        }
+
+        public void Destroy()
+        {
+            UnityEngine.Object.Destroy(this);
+        }
+    }
+
 }
